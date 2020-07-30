19d3e12fe9ad9b31368c3cf325319441693bb3e2
vkolesnik017 <vkolesnik017@gmail.com>
2020-07-31 02:40:22 +0300

    update mainPage

--- src/main/java/abTest/ListingOfHotel.java
+++ src/main/java/abTest/ListingOfHotel.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class ListingOfHotel {
     WebDriverWait wait = new WebDriverWait(WebDriverProvider.getWebDriver(), 5);
@@ -16,15 +19,38 @@ public class ListingOfHotel {
     @FindBy(how = How.XPATH, xpath = "//ul[@class='sort_option_list sort_option_list--a11y']/li")
     List<WebElement> filterLinks;
 
-    @FindBy(how = How.XPATH, xpath = "//*[contains(text(),'Получите доступ к более низким ценам')]")
+    @FindBy(how = How.XPATH, xpath = "//div[@class='sr-usp-overlay__title']")
     WebElement loader;
+
+    @FindBy(how = How.XPATH, xpath = "//span[contains(@class,'sr-hotel__name')]")
+    List<WebElement> titleOfHotels;
+
+    @FindBy(how = How.XPATH, xpath = "//div[@class='bui-price-display__value prco-text-nowrap-helper prco-inline-block-maker-helper']")
+    List<WebElement> priceList;
 
 
     public ListingOfHotel selectPriceSorting() {
-        wait.until(ExpectedConditions.visibilityOf(filterBlock));
+        String firstCurrentHotel;
+        wait.until(visibilityOf(filterBlock));
+        firstCurrentHotel = titleOfHotels.get(0).getText();
         filterLinks.get(2).click();
+       // wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//span[contains(@class,'sr-hotel__name')]"), firstCurrentHotel));
         wait.until(ExpectedConditions.visibilityOf(loader));
-        wait.until(ExpectedConditions.not(ExpectedConditions.visibilityOf(loader)));
+        wait.until(ExpectedConditions.invisibilityOf(loader));
         return this;
     }
+
+    public ListingOfHotel checkSortingPrice() {
+         List<Integer>priceBeforeSorting = new ArrayList<Integer>();
+
+         for (int i=0;i<priceList.size();i++) {
+             priceBeforeSorting.add(Integer.parseInt(priceList.get(i).getText().replaceAll("[^0-9]","")));
+         }
+
+         for (int e:priceBeforeSorting){
+             System.out.println(e);
+         }
+        return this;
+    }
+
 }
--- src/test/java/abTest/FiltersByTags.java
+++ src/test/java/abTest/FiltersByTags.java
@@ -4,14 +4,11 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 public class FiltersByTags extends BaseTest {
-
-
     @Test
     public void checkOfFiltersTagsSorting() {
-
         MainPage mainPage = PageFactory.initElements(WebDriverProvider.getWebDriver(), abTest.MainPage.class);
         ListingOfHotel listingPage = mainPage.selectСity().selectDate();
-        listingPage.selectPriceSorting();
+        listingPage.selectPriceSorting().checkSortingPrice();
         System.out.println();
 
     }
